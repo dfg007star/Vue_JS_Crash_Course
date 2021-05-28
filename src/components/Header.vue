f5700c962554c9f2cbb8257398026b9d4155b397
dfg007star <lastfm4321@gmail.com>
2021-05-28 23:42:20 +0300

    initialize

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button v-show="homePage"
+      @btn-click="$emit('btn-click')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'" />
+  </header>
+</template>
+
+<script>
+  import Button from './Button';
+
+  export default {
+    name: 'Header',
+    components: {
+      Button
+    },
+    computed: {
+      homePage() {
+        if(this.$route.path === '/') {
+          return true
+        } else {
+          return false
+        }
+      }
+    },
+    props: {
+      title: String,
+      showAddTask: Boolean
+    }
+  }
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
